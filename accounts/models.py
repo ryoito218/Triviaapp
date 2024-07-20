ee742433bcda253bcf93a741d41021ce94c21fd6
ryoito218 <ryoito218@gmail.com>
2024-07-20 21:37:51 +0900

    Create model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,67 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
-# Create your models here.
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None):
+        if not email:
+            raise ValueError('Users must have an email address')
+        user = self.model(
+            email = self.normalize_email(email),
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    def create_staffuser(self, email, password=None):
+        user = self.create_user(
+            email=email,
+            password=password,
+        )
+        user.is_active = True
+        user.is_staff = True
+        user.save(using=self._db)
+        return user
+    
+    def create_superuser(self, email, password=None):
+        user = self.create_user(
+            email=email,
+            password=password,
+        )
+        user.is_active = True
+        user.is_staff = True
+        user.save(using=self._db)
+        return user
+    
+    def create_superuser(self, email, password=None):
+        user = self.create_user(
+            email=email,
+            password=password,
+        )
+        user.is_active = True
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self.db)
+        return user
+
+class User(AbstractBaseUser):
+    email = models.EmailField(
+        verbose_name='Email address',
+        max_length=255,
+        unique=True,
+    )
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.email
+    
+    def has_perm(self, perm, obj=None):
+        return self.is_superuser
+    
+    def has_module_perms(self, app_label):
+        return self.is_superuser
