65208c8df126172a66f9f32e28347a49674df5cb
ryoito218 <ryoito218@gmail.com>
2024-07-21 01:19:37 +0900

    Create template list

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import HomeView
+from .views import HomeView, TriviaListView
 
 app_name = "main"
 
 urlpatterns = [
+    path('<str:prefecture>/', TriviaListView.as_view(), name="trivia-list"),
     path('', HomeView.as_view(), name="home"),
 ]
--- main/views.py
+++ main/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
+from .models import Post
 
 class HomeView(TemplateView):
     template_name = "main/home.html"
+
+class TriviaListView(ListView):
+    template_name = "trivia_list.html"
+
+    def get_queryset(self):
+        prefecture = self.kwargs['prefecture']
+        trivias = Post.objects.filter(prefecture=prefecture)
+        return trivias
