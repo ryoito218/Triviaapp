1e6b6f2b5e7bb213dd91ccda25c433ac122d251c
ryoito218 <ryoito218@gmail.com>
2024-07-20 21:38:23 +0900

    Create admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-# Register your models here.
+from .models import User
+
+class UserAdmin(BaseUserAdmin):
+    list_display = (
+        'email',
+        'is_active',
+        'is_staff',
+        'is_superuser',
+    )
+    list_filter = (
+        'is_superuser',
+        'is_active',
+    )
+    ordering = ('email',)
+    filter_horizontal = ()
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2')}
+        ),
+    )
+
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Permissions', {'fields': ('is_staff', 'is_superuser',)}),
+    )
+
+admin.site.register(User, UserAdmin)
