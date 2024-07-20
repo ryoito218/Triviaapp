0a6d0ea24a0b5bfef189d697fe5bc491e34e637b
ryoito218 <ryoito218@gmail.com>
2024-07-21 00:19:53 +0900

    Create main model

--- main/models.py
+++ main/models.py
@@ -1,2 +1,36 @@
 from django.db import models
+from accounts.models import User
 
+class Post(models.Model):
+    title = models.CharField(max_length=20)
+    category = models.CharField(max_length=20)
+    prefecture = models.CharField(max_length=20)
+    content = models.TextField(blank=True, null=True)
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    create_at = models.DateTimeField()
+    update_at = models.DateTimeField()
+
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        db_table = 'posts'
+        ordering = ["-create_at"]
+
+class Comment(models.Model):
+    content = models.TextField()
+    
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+    create_at = models.DateTimeField()
+    update_at = models.DateTimeField()
+
+    def __str__(self):
+        return self.create_at
+    
+    class Meta:
+        db_table = 'comments'
+        ordering = ["-create_at"]
