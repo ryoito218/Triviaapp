d7dbb49cbe4b2bdd4f3be78d8161b14a8be9b033
ryoito218 <ryoito218@gmail.com>
2024-07-21 17:40:23 +0900

    Change something

--- main/models.py
+++ main/models.py
@@ -92,8 +92,7 @@ class Comment(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
 
-    create_at = models.DateTimeField()
-    update_at = models.DateTimeField()
+    create_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.create_at
